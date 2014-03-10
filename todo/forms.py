798f63421d3683a61c049752d3dee25d64a15296
erictempleton1 <erictempleton1@gmail.com>
2014-03-09 21:18:48 -0400

    major changes. sorted out sessions, and g.user. Also moved signup form validation into views, and made slight changes to templates. Mostly ready for actual list db work now, I think.

--- todo/forms.py
+++ todo/forms.py
@@ -7,18 +7,6 @@ class Signup(Form):
     email = TextField('Email', [validators.Required('Please enter your email'), validators.Email('Please enter a valid email address')])
     password = PasswordField('Password', [validators.Required('Please choose a password')])
 
-    def __init__(self, *args, **kwargs):
-        Form.__init__(self, *args, **kwargs)
-
-    def validate(self):
-        if not Form.validate(self):
-            return False
-        if User.query.filter_by(email = self.email.data.lower()).count() > 0:
-            self.email.errors.append('Email already in use.')
-            return False
-        else:
-            return True
-
 class UserLogin(Form):
     email = TextField('email', [validators.Required('Please enter your email address'), validators.Email('Please enter a valid email address')])
     password = PasswordField('password', [validators.Required('Please enter your password')])
--- todo/views.py
+++ todo/views.py
@@ -5,28 +5,54 @@ from models import User
 from todo import app, db, login_manager
 from werkzeug.security import check_password_hash
 
+@app.before_request
+def load_user():
+    try:
+        g.user = User.query.filter_by(email=session['user_email']).first()
+    except Exception:
+        g.user = None
 
 @app.route('/')
 def index():
-    return render_template('base.html', title='Home')
+    try:
+        user_name = g.user.name
+    except AttributeError: # error thrown when user is not logged in
+        user_name = 'Guest'
+
+    return render_template('base.html', user=user_name)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = Signup()
 
-    if request.method == 'POST' and form.validate() == False or request.method == 'GET':
-        return render_template('signup.html', form=form, title='Signup')
-
-    else:
-        newuser = User(form.name.data, form.email.data, form.password.data)
-        db.session.add(newuser)
-        db.session.commit()
+    if 'logged_in' in session:
+        flash('You are already signed up! Please log out to create a new account.')
         return redirect('/')
+
+    if request.method == 'POST' and form.validate_on_submit() == False or request.method == 'GET':
+        return render_template('signup.html', form=form)
+
+    if request.method == 'POST' and form.validate_on_submit():
+
+        if User.query.filter_by(email=form.email.data.lower()).first() is not None:
+            flash('Email already in use.')
+            return render_template('signup.html', form=form)
+
+        else:
+            newuser = User(form.name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            flash('Account created! Please login.')
+            return redirect('/')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = UserLogin()
+
+    if 'logged_in' in session:
+        flash('You are already logged in! Please log out to sign in as another user.')
+        return redirect('/')
 
     if request.method == 'POST' and form.validate_on_submit() == False or request.method == 'GET':
         return render_template('login.html', form=form, title='Login')
@@ -54,16 +80,23 @@ def login():
             check_pw = check_email.check_password(user_pw)
             if check_pw is True:
                 session['logged_in'] = True
+                session['user_email'] = form.email.data
                 flash('Logged in!')
                 # login_user(check_email)
-                return redirect('/')
+                user_id = check_email.id
+                return redirect(url_for('index'))
 
     return render_template('login.html', form=form, title='Login')
 
+@app.route('/lists')
+def user_lists():
+    return none
+    
         
-@app.route("/logout")
+@app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user_email', None)
 
     # gets rid of flashed message from prev session
     session.pop('_flashes', None)

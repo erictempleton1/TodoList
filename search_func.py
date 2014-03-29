362cf3bf45b9901dda89e0e4e9c0a61acedf9719
erictempleton1 <erictempleton1@gmail.com>
2014-03-28 20:41:30 -0400

    search testing initial commit

---
+++ search_func.py
@@ -0,0 +1,9 @@
+search_list = ['Pick up eggs', 'Grab some bread', 'Bank statement']
+
+new_list = []
+for items in search_list:
+    new_list.append(items.split())
+
+for item in new_list:
+    if 'some' in item:
+        print item

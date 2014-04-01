9584e9757ff44d3c1496c16848f09fccd089dd4e
erictempleton1 <erictempleton1@gmail.com>
2014-03-31 20:04:29 -0400

    db changes for sub list

--- todo/models.py
+++ todo/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     signup_date = db.Column(db.DateTime)
     pw_hash = db.Column(db.String(64))
-    user_list = db.relationship('UserTodo', backref = 'user', lazy = 'dynamic')
+    user_list = db.relationship('TodoName', backref = 'user', lazy = 'dynamic')
 
     def __init__(self, name, email, password):
         self.name = name.title()
@@ -25,13 +25,28 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.name)
 
+class TodoName(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    todo_name = db.Column(db.String(140))
+    todo_status = db.Column(db.String)
+    todo_date = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sub_list = db.relationship('UserTodo', backref = 'sub_list', lazy = 'dynamic')
+
+    def __init__(self, todo_name, todo_status):
+        self.todo_name = todo_name.lower()
+        self.todo_status = 'active'
+
+    def __ref__(self):
+        return '<TodoName %r>' % (self.todo_name)
+
 class UserTodo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     todo_item = db.Column(db.String)
     item_due_date = db.Column(db.String(10))
     todo_item_note = db.Column(db.String(300))
     create_date = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    todo_id = db.Column(db.Integer, db.ForeignKey('sub_list.id'))
 
     def __repr__(self):
         return '<UserTodo %r>' % (self.todo_item)
